3e07639a221a2fef4791d69c3d79cc9e47ceb500
Hiroto Hasegawa <hiroto.hasegawa.0828@gmail.com>
2022-06-28 22:01:16 +0900

    feat: property の set, get を追加した

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,21 @@
 const main = (): void => {
   console.log("hello");
 };
+
+// eslint-disable-next-line no-unused-vars
+const setProperties = (): void => {
+  // // NATURE_ACCESS_TOKEN に サイトで発行した access_token を set する
+  // const scriptProperties = PropertiesService.getScriptProperties();
+  // scriptProperties.setProperties({ NATURE_ACCESS_TOKEN: "{{access_token}}" });
+};
+
+// eslint-disable-next-line no-unused-vars
+const getProperties = (): void => {
+  const scriptProperties = PropertiesService.getScriptProperties();
+  const properties = scriptProperties.getProperties();
+
+  // eslint-disable-next-line no-restricted-syntax, guard-for-in
+  for (const key in properties) {
+    Logger.log("キー: %s, 値: %s", key, properties[key]);
+  }
+};
